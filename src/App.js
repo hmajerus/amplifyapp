024bdcac95781b2b1350575b4b63ef518764ad6e
hmajerus <hmajerus2@gmail.com>
2021-01-08 14:18:03 -0600

    Fix stylings

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <img src="https://trackercdn.com/cdn/tracker.gg/valorant/icons/tiers/14.png" style="width: 160px"/>
-          <img src="https://trackercdn.com/cdn/tracker.gg/rocket-league/ranks/s4-14.png" style="width: 160px"/>
-          <img src="https://cdn.mobalytics.gg/assets/lol/images/rank-icon/helm/silver_3.png" style="width: 160px"/>
+          <img src="https://trackercdn.com/cdn/tracker.gg/valorant/icons/tiers/14.png" style={{width: '160px'}}/>
+          <img src="https://trackercdn.com/cdn/tracker.gg/rocket-league/ranks/s4-14.png" style={{width: '160px'}}/>
+          <img src="https://cdn.mobalytics.gg/assets/lol/images/rank-icon/helm/silver_3.png" style={{width: '160px'}}/>
         </p>
         <h1>Ma name harry</h1>
       </header>
